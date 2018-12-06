f8bd27c234cf156b6acaf42ae0d69e2d56a05583
albertadevor <alberta.devor@gmail.com>
2018-12-06 12:10:19 -0500

    resized maintaining shapes

--- change_dist.m
+++ change_dist.m
@@ -5,11 +5,15 @@
 % new_dist is how far away we want the object to be
 
 masked = mask_image('templeSR0006.png');
-imshow(change(masked, 5, 10))
+imshow(change(masked, 5, 7))
 
-function im = change(masked_image, orig_dist, new_dist)
+function out = change(masked_image, orig_dist, new_dist)
     scale = orig_dist / new_dist;
-    im = zeros(size(masked_image));
-    
-
+    out = zeros(size(masked_image));
+    small = imresize(masked_image, scale);
+    padding = (size(out) - size(small)) / 2;
+    pceil = ceil(padding);
+    pfloor = floor(padding);
+    out = padarray(small, pfloor, 'post');
+    out = padarray(out, pceil, 'pre');
 end
