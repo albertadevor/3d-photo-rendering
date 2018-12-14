328138da40a2268bb3c992e775e8408b19dafcea
Gary Chien <garychien1@gmail.com>
2018-12-13 22:48:41 -0500

    added script to extract specific angles

---
+++ ARTags/webcam_ar_track/scripts/select_angles.py
@@ -0,0 +1,41 @@
+import cv2, os, csv, shutil, argparse
+
+def select_angles(angles_list):
+    for desired_angle in angles_list:
+        get_data(desired_angle)
+
+def get_data(desired_angle):
+    with open('data.csv') as csv_file:
+        reader = csv.reader(csv_file)
+        best_framenum = None
+        best_angle = None
+        best_dist = None
+        best_angle_diff = 100000
+        for row in reader:
+            curr_framenum = row[0]
+            curr_angle = float(row[1])
+            curr_dist = float(row[2])
+            curr_angle_diff = abs(curr_angle - desired_angle)
+            if best_angle is None or (curr_angle_diff < best_angle_diff):
+                best_angle_diff = curr_angle_diff
+                best_angle = curr_angle
+                best_dist = curr_dist
+                best_framenum = curr_framenum
+    framepath = 'frame' + best_framenum.zfill(5) + '.png'
+    assert framepath in os.listdir('masked_frames')
+    img = cv2.imread('masked_frames/' + framepath)
+    assert img is not None
+    cv2.imwrite('extracted_frames/angle_{}_dist_{}.png'.format(int(best_angle*10), int(best_dist*10)), img)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--angles', required=True, help='desired angles, seperated by commas')
+    args = vars(parser.parse_args())
+    input_angles = args['angles']
+    angles = [float(a) for a in input_angles.split(',')]
+    if os.path.isdir('extracted_frames'):
+        shutil.rmtree('extracted_frames')
+    os.mkdir('extracted_frames')
+    select_angles(angles)
+    print 'frames extracted to extracted_frames/'
